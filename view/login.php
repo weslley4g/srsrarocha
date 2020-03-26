e5c287cd42202eb21bba28de650fbb1db5e209ed
weslley zanirate <zanirate90@gmail.com>
2020-03-26 10:17:04 -0300

    ok

--- view/login.php
+++ view/login.php
@@ -78,43 +78,44 @@ require_once('../conexao/conexao.php');
 
 
 <div class="limiter">
-		<div class="container-login100" style="background-image: url('images/bg-01.jpg');">
+		<div class="container-login100" style="background-image: url('https://pixelz.cc/wp-content/uploads/2018/08/hamburger-spicy-uhd-4k-wallpaper.jpg');">
 			<div class="wrap-login100">
-				<form class="login100-form validate-form">
+				<form class="login100-form validate-form" method="POST" action="./controller/">
 					<span class="login100-form-logo">
-						<i class="zmdi zmdi-landscape"></i>
+						<img src="./images/fastfood48dp.svg" alt="">
+						
 					</span>
 
 					<span class="login100-form-title p-b-34 p-t-27">
-						Log in
+					whatsfood
 					</span>
 
-					<div class="wrap-input100 validate-input" data-validate = "Enter username">
-						<input class="input100" type="text" name="username" placeholder="Username">
+					<div class="wrap-input100 validate-input" data-validate = "Digite seu E-mail">
+						<input class="input100" type="text" name="E-mail" placeholder="E-mail">
 						<span class="focus-input100" data-placeholder="&#xf207;"></span>
 					</div>
 
-					<div class="wrap-input100 validate-input" data-validate="Enter password">
-						<input class="input100" type="password" name="pass" placeholder="Password">
+					<div class="wrap-input100 validate-input" data-validate="Digite sua Senha">
+						<input class="input100" type="Senha" name="pass" placeholder="Senha">
 						<span class="focus-input100" data-placeholder="&#xf191;"></span>
 					</div>
 
 					<div class="contact100-form-checkbox">
 						<input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
 						<label class="label-checkbox100" for="ckb1">
-							Remember me
+							Salvar entrada
 						</label>
 					</div>
 
 					<div class="container-login100-form-btn">
 						<button class="login100-form-btn">
-							Login
+							Entrar
 						</button>
 					</div>
 
 					<div class="text-center p-t-90">
 						<a class="txt1" href="#">
-							Forgot Password?
+							Esqueceu sua senha?
 						</a>
 					</div>
 				</form>
